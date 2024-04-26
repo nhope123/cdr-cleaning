28270b2caed465696554d04c24761c3ea242a9fa
Nial Hope <nyevincy@gmail.com>
2024-04-26 04:15:24 -0400

    update deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
